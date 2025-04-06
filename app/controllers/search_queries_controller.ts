d40d69a5f93b52ef866223b5b585cea01e8861b6
Kasiastyy <eyziii123@gmail.com>
2025-04-06 18:48:35 +0200

    feat: search query controller update

--- app/controllers/search_queries_controller.ts
+++ app/controllers/search_queries_controller.ts
@@ -1,11 +1,13 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 import { HttpContext } from "@adonisjs/core/http";
 
+import SearchQuery from "#models/search_query";
 import { createSearchQueryValidator } from "#validators/search_query";
 
 export default class SearchQueriesController {
   async store({ request }: HttpContext) {
-    const data = request.validateUsing(createSearchQueryValidator);
-    return data;
+    const data = await request.validateUsing(createSearchQueryValidator);
+    const searchQuery = await SearchQuery.create(data);
+    return searchQuery;
   }
 }
--- database/migrations/1743951932838_create_search_queries_table.ts
+++ database/migrations/1743951932838_create_search_queries_table.ts
@@ -1,12 +1,13 @@
 import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = "saerch_queries";
+  protected tableName = "search_queries";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
 
+      table.string("name");
       table.float("price_min");
       table.float("price_max");
 
